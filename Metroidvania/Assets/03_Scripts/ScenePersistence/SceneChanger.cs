2efc252c93829cd25e84ad84bf0d6e002b0edc9f
Julen <jmontes@gaming.tech>
2026-07-31 21:02:29 +0200

    SceneManagement

---
+++ Metroidvania/Assets/03_Scripts/ScenePersistence/SceneChanger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void ChangeSceneNow()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
+    
